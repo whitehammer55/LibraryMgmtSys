90846e370d426db64b85acfded530bffe9f9590d
vinay-deshmukh <vinay_deshmukh@outlook.com>
2018-09-18 19:51:13 +0530

    Populate books table
    Add insertion of book data
    
    Mov json to separate folder
    
    Print only bookID, title instead of full cmd
    
    Print drop/creation desc instead of all commands
    
    Rename curated-books.json to books.json

--- populate_db.py
+++ populate_db.py
@@ -1,6 +1,7 @@
 import MySQLdb
+import json
 
-create_tables = """
+ddl_create = """
 DROP TABLE IF EXISTS users ;
 DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS books;
@@ -9,7 +10,7 @@ DROP TABLE IF EXISTS b_author;
 CREATE TABLE Users(UserID int NOT NULL PRIMARY KEY, Password varchar(30), Email varchar(30), DOB date, FirstName varchar(30), LastName varchar(30));
 CREATE TABLE Employees(EmployeeID int NOT NULL PRIMARY KEY, Post varchar(30), FirstName varchar(30), LastName varchar(30), Email varchar(30), DOB date, Password varchar(30));
 CREATE TABLE U_Contact(Contact varchar(30) NOT NULL UNIQUE, UserID int REFERENCES Users(UserID));
-CREATE TABLE Books(BookID int NOT NULL PRIMARY KEY, ISBN varchar(30), Title varchar(30),Edition int, UserId int REFERENCES Users(UserID), DOI date, DOR date, reissue_count int, EmployeeID int REFERENCES Employees(EmployeeID));
+CREATE TABLE Books(BookID int NOT NULL PRIMARY KEY, ISBN varchar(30), Title varchar(100),Edition int, UserId int REFERENCES Users(UserID), DOI date, DOR date, reissue_count int, EmployeeID int REFERENCES Employees(EmployeeID));
 CREATE TABLE B_Author(BookID int REFERENCES Books(BookID), AuthorName varchar(50) UNIQUE);
 """
 
@@ -17,25 +18,52 @@ CREATE TABLE B_Author(BookID int REFERENCES Books(BookID), AuthorName varchar(50
 
 
 # Connect
-db = MySQLdb.connect(host='localhost',
+conn = MySQLdb.connect(host='localhost',
                         user='root', passwd='',
                         db='wdl')
-cursor = db.cursor()
+cursor = conn.cursor()
 # cursor.execute("show tables; ")
 # data = cursor.fetchall()
 # for row in data:
 #     print(row)
-# db.close()
+# conn.close()
+
+# LOAD BOOK JSON
+def c_table_books(filename):
+    insert_fmt = \
+    """INSERT INTO Books (BookID, ISBN, Title, Edition)
+    VALUES ('{}', '{}', '{}', '{}');"""
+
+    with open(filename) as f:
+        for book in json.load(f):
+            cmd  = insert_fmt.format(
+                book['ID'],
+                book['ISBN'], 
+                book['Title'], 
+                book['Edition'])
+            print('Inserting: {:5d}: {:s}'.format(book['ID'], book['Title']))
+            cursor.execute(cmd)
+
+    print("COMMIT;")
+    conn.commit()
 
 def main():
     
     # CREATE THE DATABASE
-    for cs in create_tables.split('\n')[1:-1]:
+    for cs in ddl_create.split('\n')[1:-1]:
         # first and last part are empty, they give error
 
-        print(cs)
+        # print(cs)
         cursor.execute(cs)
+
+    print("Dropped and Created Tables!")
+    print()
+
+    # INSERT BOOKS
+    c_table_books('json/curated-books.json')
+
+
 
 if __name__ == '__main__':
     main()
-    db.close()
+    conn.close()
